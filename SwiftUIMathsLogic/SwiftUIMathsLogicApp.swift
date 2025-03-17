eb392b2c9f431f9fd2033083b6be7ef76913470f
Swati Gautam <swati.g@turing.com>
2025-03-17 19:39:55 +0530

    Initial Commit

---
+++ SwiftUIMathsLogic/SwiftUIMathsLogicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMathsLogicApp.swift
+//  SwiftUIMathsLogic
+//
+//  Created by Ruchi Kumar on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMathsLogicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
